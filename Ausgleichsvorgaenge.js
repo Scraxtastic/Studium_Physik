1b84c5ce4c2b0dccf3b7eec1f7ffb09e7efa1d20
Scraxtastic <nick.u19990@gmail.com>
2021-12-13 11:14:09 +0100

    Ausgleichsvorgänge Aufgabe 1

--- Ausgleichsvorgaenge.js
+++ Ausgleichsvorgaenge.js
@@ -5,3 +5,20 @@
  * C = Kapazität
  *
  */
+
+
+// UC(t) = UEnde - (UEnde - UAnfang) * e^(-t/T);
+UC = (t) => UEnde - (UEnde - UAnfang) * Math.pow(Math.E, -t / T);
+IC = (t) => IEnde - (IEnde - IAnfang) * Math.pow(Math.E, -t/T );
+
+R = 3.3 * Math.pow(10, 3); //Ohm
+C = 680 * Math.pow(10, -9); //nF (nano Farad)
+U0 = 1.5; //V
+
+T = R * C;
+t = 0;
+
+UEnde = 50;
+UAnfang = 100;
+console.log("T", T);
+console.log("UCt", UC(0), UC(T));
